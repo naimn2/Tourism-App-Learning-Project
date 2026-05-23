8122d07609656ab946b546687cfb671c8ee30d23
Naim <muflihunnaim275@gmail.com>
2026-05-24 00:57:35 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/tourismapp/core/ui/detail/DetailTourismActivity.kt
@@ -0,0 +1,63 @@
+package com.dicoding.tourismapp.core.ui.detail
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.IntentCompat.getParcelableExtra
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.dicoding.tourismapp.MyApplication
+import com.dicoding.tourismapp.R
+import com.dicoding.tourismapp.core.domain.model.Tourism
+import com.dicoding.tourismapp.databinding.ActivityDetailTourismBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class DetailTourismActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailTourismBinding
+
+    private val detailTourismViewModel: DetailTourismViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailTourismBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        val detailTourism = getParcelableExtra(intent, EXTRA_DATA, Tourism::class.java)
+        showDetailTourism(detailTourism)
+    }
+
+    private fun showDetailTourism(detailTourism: Tourism?) {
+        detailTourism?.let {
+            supportActionBar?.title = detailTourism.name
+            binding.contentDetailTourism.tvDetailDescription.text = detailTourism.description
+            Glide.with(this@DetailTourismActivity)
+                .load(detailTourism.image)
+                .into(binding.ivDetailImage)
+
+            var statusFavorite = detailTourism.isFavorite
+            setStatusFavorite(statusFavorite)
+            binding.fab.setOnClickListener {
+                statusFavorite = !statusFavorite
+                detailTourismViewModel.setFavoriteTourism(detailTourism, statusFavorite)
+                setStatusFavorite(statusFavorite)
+            }
+        }
+    }
+
+    private fun setStatusFavorite(statusFavorite: Boolean) {
+        if (statusFavorite) {
+            binding.fab.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_favorite_white))
+        } else {
+            binding.fab.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_not_favorite_white))
+        }
+    }
+
+    companion object {
+        const val EXTRA_DATA = "extra_data"
+    }
+}
---
+++ app/src/main/java/com/dicoding/tourismapp/core/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,61 @@
+package com.dicoding.tourismapp.core.ui.favorite
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dicoding.tourismapp.MyApplication
+import com.dicoding.tourismapp.core.ui.TourismAdapter
+import com.dicoding.tourismapp.databinding.FragmentFavoriteBinding
+import com.dicoding.tourismapp.core.ui.detail.DetailTourismActivity
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class FavoriteFragment : Fragment() {
+
+    private var _binding: FragmentFavoriteBinding? = null
+    private val binding get() = _binding!!
+
+    private val favoriteViewModel: FavoriteViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (activity != null) {
+
+            val tourismAdapter = TourismAdapter()
+            tourismAdapter.onItemClick = { selectedData ->
+                val intent = Intent(activity, DetailTourismActivity::class.java)
+                intent.putExtra(DetailTourismActivity.EXTRA_DATA, selectedData)
+                startActivity(intent)
+            }
+
+            favoriteViewModel.favoriteTourism.observe(viewLifecycleOwner) { dataTourism ->
+                tourismAdapter.submitList(dataTourism)
+                binding.viewEmpty.root.visibility =
+                    if (dataTourism.isNotEmpty()) View.GONE else View.VISIBLE
+            }
+
+            with(binding.rvTourism) {
+                layoutManager = LinearLayoutManager(context)
+                setHasFixedSize(true)
+                adapter = tourismAdapter
+            }
+        }
+    }
+}
